ac817314f44de5fab4e88a979a3fbdea3e1d9597
Nshuti Yvan <nshutiyvan@gmail.com>
2019-12-03 06:19:05 -0800

    working pn manage rumble

--- main.c
+++ main.c
@@ -13,6 +13,8 @@
 
 int sendData(unsigned char sendData[],libusb_device_handle *handler,int size);
 uint8_t* receiveData(libusb_device_handle *handler,int size);
+int manageLights(libusb_device_handle *handler,uint8_t packet);
+int manageRumble(libusb_device_handle *handler,uint8_t packet);
 
 int main(int argc, char *argv[])
 {
@@ -20,9 +22,7 @@ int main(int argc, char *argv[])
 	char* s1=argv[1];
 	char* s2=argv[2];
 	libusb_init(NULL);
-	libusb_device_handle *h =NULL;
-	unsigned char data[] = { 1, 3,0x00};
-	unsigned char rumble[] = {0x0,0x8,0x0,0xFF,0xFF,0x0,0x0,0x0};
+	libusb_device_handle *h =NULL;	
 	unsigned char receive[20];
 	printf("New program\n");
 	h = libusb_open_device_with_vid_pid(NULL,deviceVendorID, deviceProductID);
@@ -32,14 +32,30 @@ int main(int argc, char *argv[])
 				int received;
 				int r = libusb_interrupt_transfer(h, readEndpointAddress ,receive,sizeof(receive), &received, 0);				
 				if(r == 0){
-					if(receive[3] == 0x10)	
-						printf("Pressed A \n");
-					else if(receive[3] == 0x20)	
-						printf("Pressed B \n");
-					else if(receive[3] == 0x40)
-						printf("Pressed X \n");
-					else if(receive[3] == 0x80)
-						printf("Pressed Y \n");					
+					if(receive[3] == 0x10){
+						//printf("Pressed A \n");
+						manageLights(h,(uint8_t)0x0A);
+					}							
+					else if(receive[3] == 0x20){
+						//printf("Pressed B \n");
+						manageLights(h,(uint8_t)0x04);
+						
+					}	
+					else if(receive[3] == 0x40){
+						//printf("Pressed X \n");
+						manageLights(h,(uint8_t)0x09);
+						
+					}						
+					else if(receive[3] == 0x80){
+						//printf("Pressed Y \n");
+						manageLights(h,(uint8_t)0x08);
+					
+					}
+					else if(receive[3] == 0x10 && receive[2]==0x04){
+						manageRumble(h,(uint8_t)0xFF);
+					}
+					printf("\n");
+					printf("%x \t",receive[2]);					
 				}
 		}					
 								
@@ -51,12 +67,15 @@ int main(int argc, char *argv[])
 }
 int sendData(unsigned char sendData[],libusb_device_handle *handler,int size){
 	int transferred;
-	int send_status = libusb_interrupt_transfer(handler, writeEndpointAddress ,sendData,size, &transferred, 0);
-		if(send_status == 0 )
-			printf("Done sending data");
-		else
-			printf("Failed sending");
-	return send_status;			
+	return libusb_interrupt_transfer(handler, writeEndpointAddress ,sendData,size, &transferred, 0);			
+}
+int manageLights(libusb_device_handle *handler,uint8_t packet){
+	unsigned char message[] = {1, 3,packet};
+	return sendData(message,handler,sizeof(message));
+}
+int manageRumble(libusb_device_handle *handler,uint8_t packet){
+	unsigned char message[] = {0x0,0x8,0x0,packet,0xFF,0x0,0x0,0x0};
+	return sendData(message,handler,sizeof(message));
 }
 uint8_t* receiveData(libusb_device_handle *handler,int size){
 		uint8_t* receive=malloc(sizeof(uint8_t)*20);
